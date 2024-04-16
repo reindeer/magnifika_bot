3cc7aca561db38ff497f1bde63d1f3fff98d6328
Denis Daschenko <reindeer@tarandro.ru>
2024-04-16 21:16:13 +0300

    Get database path from argv

--- config/config.go
+++ config/config.go
@@ -1,5 +1,21 @@
 package config
 
+import (
+	"os"
+
+	"github.com/pborman/getopt/v2"
+)
+
 func Init() {
+	var (
+		db string
+	)
+	getopt.FlagLong(&db, "db", 'd', "Specify path to registry database")
+	_ = getopt.Getopt(nil)
+	if db != "" {
+		_ = os.Setenv("DB_PATH", db)
+	}
+	os.Args = append([]string{getopt.CommandLine.Program()}, getopt.Args()...)
+
 	InitDi()
 }
